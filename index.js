5c16115108f22950934c7582797d5643372000ac
mayank387 <146574492+mayank387@users.noreply.github.com>
2023-10-01 18:11:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function equals(){
+    
+    if(document.getElementById("result").innerHTML = eval(document.getElementById("calcu").innerHTML)){
+        document.getElementById("result").innerHTML = eval(document.getElementById("calcu").innerHTML)
+    }else{
+        document.getElementById("result").innerHTML = eval(document.getElementById("calcu").innerHTML)
+    }
+}
